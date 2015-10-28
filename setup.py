6ef7f3ce6f2ef68c89e9950d58d37b42285413d9
m09 <mog@crydee.eu>
2015-10-28 16:39:51 +0100

    didn't get it right the first time: adding lxml as a dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from distutils.core import setup
 
-install_requires = ['lxml']
-    
 setup(name='MinimalCoreNLPParser',
       version='1.0',
       description='Minimal CoreNLP XML Parser in Python',
       author='Florian Boudin',
       author_email='florian.boudin@univ-nantes.fr',
       packages=['corenlp_parser'],
-      url="https://github.com/boudinfl/corenlp_parser"
+      url="https://github.com/boudinfl/corenlp_parser",
+      install_requires=['lxml']
      )
